79037ccd6dbd75ce4a3e29d73ed55b191146192e
julissaAo <112599287+julissaAo@users.noreply.github.com>
2022-09-02 20:23:43 -0600

    Add files via upload

---
+++ periodo2clase161.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int num(int a){
+	if(a < 0){
+		return -1;
+		 }
+		 else if(a > 0){
+		 	return 1;
+		 	 }
+		 	 else{
+		 	 	return 0;
+		 	 	 }
+		 	 	  }
+		 	 	  
+int main(){
+	int a,b;
+	cout<<"\n Ingrese numero: \n \n";
+	cin>>a;
+	cout<< "\n \n \n \t" <<num(a);
+	
+	return 0;
+ }
+	
+  
+
+	
